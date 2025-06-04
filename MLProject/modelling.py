3b5a666b12b2abbb5d17776f33d5e90dbbbd8663
Andrew Benedictus Jamesie <andrewbjamesie@yahoo.com>
2025-06-04 23:48:09 +0700

    test debug-back

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,8 +21,8 @@ from sklearn.tree import DecisionTreeClassifier
 from dotenv import load_dotenv
 load_dotenv()
 
-dagshub_token = 'd333906431f3987fbe953f7359f5cb04d2b2cc55'
-dagshub_username = 'aNdr3W03'
+dagshub_username = os.environ.get('DAGSHUB_USERNAME')
+dagshub_token = os.environ.get('DAGSHUB_TOKEN')
 dagshub_repo_name = 'diabetes-prediction'
 
 logging.basicConfig(
@@ -38,15 +38,12 @@ logging.basicConfig(
 logger = logging.getLogger('modelling.py')
 
 def mlflow_setup():
-    print(dagshub_token)
-    print(dagshub_username)
     try:
         if dagshub_username and dagshub_token:
             os.environ['MLFLOW_TRACKING_USERNAME'] = dagshub_username
             os.environ['MLFLOW_TRACKING_PASSWORD'] = dagshub_token
 
             mlflow_url = f'https://dagshub.com/{dagshub_username}/{dagshub_repo_name}.mlflow'
-            print(f"[DEBUG] MLflow tracking URI = {mlflow_url}")
             mlflow.set_tracking_uri(mlflow_url)
         else:
             raise ValueError('DagsHub Username and Token must set on the environment.')
