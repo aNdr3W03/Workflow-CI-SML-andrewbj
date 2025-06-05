0e713688691e4d33538f1f07c6449e8f51e2d228
Andrew Benedictus Jamesie <andrewbjamesie@yahoo.com>
2025-06-05 08:03:42 +0700

    Test debug

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -37,9 +37,9 @@ logging.basicConfig(
 logger = logging.getLogger('modelling_tuning.py')
 
 if dagshub_username == 'aNdr3W03':
-    print('DagsHub username is set to aNdr3W03')
+    print('DagsHub username is set to USERNAME=aNdr3W03')
 else:
-    print('DagsHub username is not set to aNdr3W03')
+    print('DagsHub username is not set to USERNAME=aNdr3W03')
 
 def mlflow_setup():
     try:
