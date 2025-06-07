6cd24696a0b230847aaf3f72222691434fd7229e
Andrew Benedictus Jamesie <andrewbjamesie@yahoo.com>
2025-06-07 23:05:22 +0700

    Update docker

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -197,11 +197,11 @@ def rf_model_tuning(X_train, X_test, y_train, y_test):
     logger.info('Starting Random Forest model tuning...')
 
     param = {
-        'n_estimators': [50, 100, 200],
-        'max_depth': [None, 10, 20, 30],
+        'n_estimators': [100, 200, 300],
+        'max_depth': [25, 50],
         'min_samples_split': [2, 5, 10],
         'min_samples_leaf': [1, 2, 4],
-        'max_features': ['sqrt', 'log2']
+        'max_features': ['sqrt']
     }
 
     rf = RandomForestClassifier(random_state=20250531)
